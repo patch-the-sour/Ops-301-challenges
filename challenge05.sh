5bd9369dde0a287a8730a2cdb55291b29d23f7c0
Langston <108202253+patch-the-sour@users.noreply.github.com>
2022-09-02 19:46:39 -0700

    Create challenge05.sh

---
+++ challenge05.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script Name:
+# Author:Graceson Langston
+# Date of last revision:09/02/22
+# Description of purpose:Log clearing
+
+
+# Declaration of variables:
+syslogVary=/var/log/syslog
+wtmpVary=/var/log/wtmp
+
+# Declaration of functions:
+
+function clear_log {
+    contents
+}
+
+
+
+# End
